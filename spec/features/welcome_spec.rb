a25165108617808670e56cb63805fdb9845105f7
derikbf <derikb@gmail.com>
2022-01-28 09:43:54 -0300

    Created Welcome Features SPEC

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 feature "Welcome", type: :feature do
-  scenario 'Mostra a mensagem de Bem-Vindo' do
-    visit('/')
-    expect(page).to have_content('Bem-Vindo')
+  # scenario 'Mostra a mensagem de Bem-Vindo' do
+  #   visit(root_path)
+  #   expect(page).to have_content('Bem-Vindo')
+  # end
+
+  scenario 'Verificar o Link Cadastro de Clientes' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
+    # expect(page).to have_selector('ul li a')
+    # expect(page).to have_link('Cadastro de Clientes')
   end
 end
