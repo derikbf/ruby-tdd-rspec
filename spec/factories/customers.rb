6847100b8e93b2dbfdb095824563f620fb5fcc2a
derikbf <derikb@gmail.com>
2022-01-28 12:55:28 -0300

    Updated Factories Customer

---
+++ spec/factories/customers.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    email { "MyString" }
+    smoker { "MyString" }
+    phone { "MyString" }
+    avatar { "MyString" }
+  end
+end
