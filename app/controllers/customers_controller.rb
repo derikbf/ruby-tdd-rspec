20f9ff81be0f724663de1f311193cd5f5a1cf08f
derikbf <derikb@gmail.com>
2022-01-28 12:52:10 -0300

    Updated Controller Customer with actions

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,23 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create 
+    @customer = Customer.new(customer_params)
+
+    if @costumer.save
+      redirect_to customer_path, notice: 'Cliente cadastrado com sucesso!'
+    else
+      render :new
+  end
+
+  # private
+
+  def customer_params
+    params.require(:customer).permit(:id, :name, :email, :smoker, :phone, :avatar)
+  end
+end
