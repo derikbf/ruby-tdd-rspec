e434903a4302478b4ff9cfbf401d0782ac002985
derikbf <derikb@gmail.com>
2022-01-28 12:55:47 -0300

    Created Features Customer

---
+++ spec/features/customer_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature "Customers", type: :feature do
+  xscenario 'Verifica Link Cadastro de Cliente' do
+    visit(root_path)
+    expect(page).to have_link('Cadastro de Clientes')
+  end
+
+  xscenario 'Verifica Link de Novo Cliente' do
+    visit(root_path)
+    click_on('Cadastro de Clientes')
+    expect(page).to have_content('Listando Clientes')
+    expect(page).to have_link('Novo Cliente')
+  end
+
+  xscenario 'Verifica Formulário de Novo Cliente' do
+    visit(customers_path)
+    click_on('Novo Cliente')
+    expect(page).to have_content('Novo Cliente')
+  end
+
+  xscenario 'Cadastro um Cliente Válido' do
+    visit(new_customer_path)
+    customer_name = Faker::Name.name
+    fill_in('customer_name', with: customer_name)
+    fill_in('customer_email', with: Faker::Internet.email)
+    fill_in('customer_phone', with: Faker::PhoneNumber.phone_number)
+    attach_file('customer_avatar', "#{Rails.root}/spec/fixtures/avatar.png")
+    choose(option: ['S', 'N'].sample)
+    click_on('Criar Cliente')
+
+    expect(page).to have_content('Cliente cadastrado com sucesso!')
+    expect(Customer.last.name).to eq(customer_name)  
+  end
+end
