f2ef80ff03b42c37dfbb775721fdf9eddea89114
derikbf <derikb@gmail.com>
2022-01-30 09:37:16 -0300

    Created Validate SPEC create and edit Customer

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -32,4 +32,64 @@ feature "Customers", type: :feature do
     expect(page).to have_content('Cliente cadastrado com sucesso!')
     expect(Customer.last.name).to eq(customer_name)  
   end
+
+  xscenario 'does somethings' do
+    visit(new_customer_path)
+    click_on('Criar Cliente')
+    expect(page).to have_content('não pode ficar em branco')  
+  end
+
+  xscenario 'Mostra um cliente' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+    visit(customer_path(customer.id))
+    expect(page).to have_content(customer.name)
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
+  end
+
+  xscenario 'Testando a index' do
+    customer1 = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    customer2 = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    expect(page).to have_content(customer1.name).and have_content(customer2.name)
+    expect(page).to have_content(customer1.email).and have_content(customer2.email)
+    expect(page).to have_content(customer1.phone).and have_content(customer2.phone)
+  end
+
+  xscenario 'Atualiza um Cliente' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S', 'N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+    new_name = Faker::Name.name
+    visit(cedit_customer_path(customer.id))
+    fill_in('Nome', with: new_name)
+    click_on('Atualizar Cliente')
+
+    expect(page).to have_content('Cliente atualizado com sucesso!')
+    expect(page).to have_content(new_name) 
+  end
 end
